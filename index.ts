54285648b7989191e7447d9cdcb6ec50d853e516
Begmatov Almaz <lolkek13@inbox.ru>
2025-01-15 12:47:29 +0600

    fixed litte bags

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import config from './config';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import config from "./config";
+import userRouter from "./roters/user";
+import taskRouter from "./roters/task";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8000;
@@ -9,18 +11,19 @@ const port = 8000;
 app.use(cors(config.corsOptions));
 app.use(express.json());
 app.use(express.static("pablic"));
+app.use("/user", userRouter);
+app.use("/task", taskRouter);
 
+const run = async () => {
+  await mongoose.connect(config.database);
 
-const run = async()=>{
-    await mongoose.connect(config.database);
+  app.listen(port, () => {
+    console.log(`Server started on ${port} port!`);
+  });
 
-    app.listen(port, ()=>{
-        console.log(`Server started on ${port} port!`);
-    });
-
-    process.on('exit', ()=>{
-        mongoose.disconnect();
-    });
+  process.on("exit", () => {
+    mongoose.disconnect();
+  });
 };
 
 run().catch(console.error);
